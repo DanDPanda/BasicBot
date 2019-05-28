1f5669d8e2b502c6bb1a1d5112fbc750626a4989
DanDPanda <dan.v.dinh@gmail.com>
2019-05-27 22:59:31 -0500

    Yes/No poll

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
-require('dotenv').config();
+require("dotenv").config();
 
 try {
   bot.login(process.env.DISCORD);
-} catch {
+} catch (e) {
   console.log("Discord is down");
 }
+
+// Message the bot displays when ready
+bot.on("ready", () => {
+  console.log("Bot Online");
+  bot.user.setActivity("the sounds of the ocean", { type: 2 });
+});
+
+// This reads the message and sends the appropriate command
+bot.on("message", message => {
+  let commandFile = require(`./message.js`);
+  commandFile.run(bot, message);
+});
