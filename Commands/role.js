8e988e4a7e3ce688fef32ec9e681a8c16150efc9
Dan Dinh <dinhx103@umn.edu>
2019-06-16 12:41:08 -0500

    Need to check if it is a PM or channel

--- Commands/role.js
+++ Commands/role.js
@@ -4,11 +4,28 @@ exports.run = async message => {
     message.content.split(" ")[0].toLowerCase() === "!role" &&
     message.content.split(" ").length === 2
   ) {
+    // Gets the string of the role name
     const target = message.content.split(" ")[1];
-    if (message.member.roles.some(r => [target].includes(r.name))) {
-      message.channel.send("True");
-    } else {
-      message.channel.send("False");
+
+    // Prevents the bot from giving access to the these roles.
+    if (target === "Admin") {
+      return;
+    }
+
+    // Gets the actual role details from the server based off of the name
+    let role = message.guild.roles.find(r => r.name === target);
+
+    // Searches
+    try {
+      if (message.member.roles.some(r => [target].includes(r.name))) {
+        message.member.removeRole(role);
+        message.author.send(`${target} was removed from your roles.`);
+      } else {
+        message.member.addRole(role);
+        message.author.send(`${target} was added to your roles.`);
+      }
+    } catch (e) {
+      console.log("Invalid Role");
     }
   }
 };
