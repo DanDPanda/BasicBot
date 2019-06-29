149ade26bfaf55a63ab07f961c1088169fa32d47
Dan Dinh <dinhx103@umn.edu>
2019-06-29 12:03:22 -0500

    Added more visual voting options

--- Commands/vote.js
+++ Commands/vote.js
@@ -1,5 +1,18 @@
 // This is what is run when the this function is called
 exports.run = async (client, message) => {
+  const icons = [
+    `\u0031\u20E3`,
+    `\u0032\u20E3`,
+    `\u0033\u20E3`,
+    "\u0034\u20E3",
+    "\u0035\u20E3",
+    "\u0036\u20E3",
+    "\u0037\u20E3",
+    "\u0038\u20E3",
+    "\u0039\u20E3",
+    "🔟"
+  ];
+
   if (message.channel.type === "dm") {
     return;
   }
@@ -9,25 +22,25 @@ exports.run = async (client, message) => {
     message.content.split(`"`)[2].split(` `).length > 1 &&
     message.content.split(`"`)[2].split(` `).length <= 10
   ) {
+    let combinedMessage = "```";
+    combinedMessage += message.content.split(`"`)[1] + "\n";
+    for (
+      let i = 0;
+      i < message.content.split(`"`)[2].split(` `).length - 1;
+      i++
+    ) {
+      combinedMessage += `${icons[i]}: ${
+        message.content.split(`"`)[2].split(` `)[i + 1]
+      }\n`;
+    }
+    combinedMessage += "```";
     message.channel
-      .send(message.content.split(`"`)[1])
+      .send(combinedMessage)
       .then(async mess => {
         try {
-          const icons = [
-            `\u0031\u20E3`,
-            `\u0032\u20E3`,
-            `\u0033\u20E3`,
-            "\u0034\u20E3",
-            "\u0035\u20E3",
-            "\u0036\u20E3",
-            "\u0037\u20E3",
-            "\u0038\u20E3",
-            "\u0039\u20E3",
-            "🔟"
-          ];
           for (
             let i = 0;
-            i < message.content.split(`"`)[2].split(` `).length;
+            i < message.content.split(`"`)[2].split(` `).length - 1;
             i++
           ) {
             await mess.react(icons[i]);
