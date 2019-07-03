4e93bc95ac26d368ff5ddc28754f0a613ff96559
DanDPanda <dan.v.dinh@gmail.com>
2019-07-03 18:38:51 -0500

    Refactored vote

--- Commands/vote.js
+++ Commands/vote.js
@@ -1,52 +1,60 @@
-// This is what is run when the this function is called
-exports.run = async (client, message) => {
-  const icons = [
-    `\u0031\u20E3`,
-    `\u0032\u20E3`,
-    `\u0033\u20E3`,
-    "\u0034\u20E3",
-    "\u0035\u20E3",
-    "\u0036\u20E3",
-    "\u0037\u20E3",
-    "\u0038\u20E3",
-    "\u0039\u20E3",
-    "🔟"
-  ];
+const icons = [
+  `\u0031\u20E3`,
+  `\u0032\u20E3`,
+  `\u0033\u20E3`,
+  "\u0034\u20E3",
+  "\u0035\u20E3",
+  "\u0036\u20E3",
+  "\u0037\u20E3",
+  "\u0038\u20E3",
+  "\u0039\u20E3",
+  "🔟"
+];
 
+const actual_icons = [
+  "1️⃣",
+  "2️⃣",
+  "3️⃣",
+  "4️⃣",
+  "5️⃣",
+  "6️⃣",
+  "7️⃣",
+  "8️⃣",
+  "9️⃣",
+  "🔟"
+];
+
+const createCombinedMessage = (question, votes) => {
+  let combinedMessage = "```\n";
+  combinedMessage += `${question}\n\n`;
+  for (let i = 0; i < votes.length - 1; i++) {
+    combinedMessage += `${actual_icons[i]}: ${votes[i + 1]}\n`;
+  }
+  combinedMessage += "```";
+  return combinedMessage;
+};
+
+exports.run = async (client, message) => {
   if (message.channel.type === "dm") {
     return;
   }
+
+  const command = message.content.split(" ")[0];
+  const question = message.content.split(`"`)[1];
+  const votes = message.content.split(`"`)[2].split(` `);
+
   if (
-    message.content.split(" ")[0].toLowerCase() === "!vote" &&
+    command.toLowerCase() === "!vote" &&
     message.content.split(`"`).length === 3 &&
-    message.content.split(`"`)[2].split(` `).length > 1 &&
-    message.content.split(`"`)[2].split(` `).length <= 10
+    votes.length > 1 &&
+    votes.length <= 10
   ) {
-    let combinedMessage = "```\n";
-    combinedMessage += `${message.content.split(`"`)[1]}\n\n`;
-    for (
-      let i = 0;
-      i < message.content.split(`"`)[2].split(` `).length - 1;
-      i++
-    ) {
-      combinedMessage += `${icons[i]}: ${
-        message.content.split(`"`)[2].split(` `)[i + 1]
-      }\n`;
-    }
-    combinedMessage += "```";
+    const combinedMessage = createCombinedMessage(question, votes);
     message.channel
       .send(combinedMessage)
       .then(async mess => {
-        try {
-          for (
-            let i = 0;
-            i < message.content.split(`"`)[2].split(` `).length - 1;
-            i++
-          ) {
-            await mess.react(icons[i]);
-          }
-        } catch (e) {
-          console.log("Bad usage of the vote");
+        for (let i = 0; i < votes.length - 1; i++) {
+          await mess.react(icons[i]);
         }
       })
       .catch(() => {
